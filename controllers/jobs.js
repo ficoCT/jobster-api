62ecd922a113609c038d1121ce70e31a574d3bba
FICO\rafal <rafalficek082@gmail.com>
2023-07-14 22:31:55 +0200

    Moment

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -2,6 +2,7 @@ const Job = require('../models/Job');
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
 const mongoose = require('mongoose');
+const moment = require('moment');
 
 const getAllJobs = async (req, res) => {
   const { search, status, jobType, sort } = req.query;
